4b3cde260ab82781adbfaf551495494093340fa9
David Radcliffe <dradcliffe@gmail.com>
2013-06-07 08:24:54 -0500

    Changed string substitution rule
    Changed the rule for removing prefixes from tags to avoid data
    corruption.

--- Radcliffe/BCA.py
+++ Radcliffe/BCA.py
@@ -16,13 +16,13 @@ class BCA():
         self.infile = infile
         cur.execute('SELECT MAX(SubjectID) FROM SubjectName')
         self.subjects = cur.fetchone()[0] or 0
-        
+
         cur.execute('SELECT MAX(ConvictionID) from ConvictionRecord')
         self.convictions = cur.fetchone()[0] or 0
-        
+
         cur.execute('SELECT MAX(SupervisionID) from SupervisionRecord')
         self.supervisions = cur.fetchone()[0] or 0
-        
+
     def run(self):
         begin_tag = "<CriminalRecord>"
         end_tag = "</CriminalRecord>"
@@ -38,16 +38,17 @@ class BCA():
                     buf += line0 + end_tag
                     if begin_tag in buf:
                         buf = begin_tag + buf.split(begin_tag,1)[1]
-                        buf = buf.replace('mnj:','').replace('j:','')
+                        buf = buf.replace('<mnj:','<').replace('<j:','<')
+                        buf = buf.replace('</mnj:','</').replace('</j:','</')
                         record = et.fromstring(buf)
-                        process_criminal_record.run(self,record)
+                        process_criminal_record.run(self, record)
                         line = line1
                     buf = ''
                     line = line1
-                        
+
                 buf = line
             else:
                 buf += line
-            
+
         self.infile.close()
 
